da6583c1eec7f5fd097db6320b2b2ed3c18f3bc4
Rodrigo Botti <rodrigo.botti@gmail.com>
2023-01-29 00:04:52 -0300

    Improve code with iterators

--- src/lib.rs
+++ src/lib.rs
@@ -18,33 +18,18 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line)
-        }
-    }
-
-    results
-
-    // contents
-    //     .lines()
-    //     .filter(|line| line.contains(query))
-    //     .collect()
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 pub struct Config {
@@ -54,20 +39,17 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
 
-        // There are a number of ways we could manage the String data;
-        // the easiest, though somewhat inefficient, route is to call the clone method on the values.
-        // This will make a full copy of the data for the Config instance to own,
-        // which takes more time and memory than storing a reference to the string data.
-        // However, cloning the data also makes our code very straightforward because we don’t have to manage the lifetimes of the references;
-        // in this circumstance, giving up a little performance to gain simplicity is a worthwhile trade-off.
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ use minigrep::Config;
 use std::{env, process};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let args = env::args();
+    let config = Config::build(args).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
